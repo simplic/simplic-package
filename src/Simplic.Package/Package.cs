5c50a411e65d96318f30c70443e31a3375cb14e5
froeh010 <froeh010@uni-hildesheim.de>
2020-09-22 13:52:39 +0200

    Add Package POCO

---
+++ src/Simplic.Package/Package.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Package
+{
+    public class Package
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public List<Dictionary<String, String>> Dependencies { get; set; }
+        public Dictionary<String, Dictionary<String, Dictionary<String, String>>> Objects { get; set; }
+    }
+}
