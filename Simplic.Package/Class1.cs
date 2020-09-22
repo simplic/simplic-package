f894f9bd7d5fa980276e2e0a6812091fc8dad3b8
froeh010 <froeh010@uni-hildesheim.de>
2020-09-22 13:36:33 +0200

    Initial commit

---
+++ Simplic.Package/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Package
+{
+    public class Class1
+    {
+    }
+}
