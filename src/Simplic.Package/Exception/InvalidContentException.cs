8a4092c1e0f1af3b4f54a0bc468c515d7387ef29
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-02 14:12:30 +0200

    comment exceptions

--- src/Simplic.Package/Exception/InvalidContentException.cs
+++ src/Simplic.Package/Exception/InvalidContentException.cs
@@ -1,25 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Simplic.Package
 {
     /// <summary>
-    /// Exception that is thrown, when there is a mismatch between accepted content types of an object and the given content type
-    /// Also thrown when there is no implementation for given content type
+    /// Exception that is thrown, when there is a mismatch between accepted content types of an object and the given content type.
+    /// Also thrown when there is no implementation for given content type.
     /// </summary>
     public class InvalidContentException : Exception
     {
+        /// <summary>
+        /// Initializes a new Simplic.Package.InvalidContentException instace.
+        /// </summary>
         public InvalidContentException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new Simplic.Package.InvalidContentException instace.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public InvalidContentException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new Simplic.Package.InvalidContentException instace.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="inner">The exception that caused this exception, or null.</param>
         public InvalidContentException(string message, Exception inner) : base(message, inner)
         {
         }
--- src/Simplic.Package/Exception/MissingObjectException.cs
+++ src/Simplic.Package/Exception/MissingObjectException.cs
@@ -3,19 +3,31 @@
 namespace Simplic.Package
 {
     /// <summary>
-    /// A Exception thrown when a object is missing from a package
-    /// Can be thrown when unpacking
+    /// A Exception thrown when a object is missing from a package.
+    /// Can be thrown when unpacking.
     /// </summary>
     public class MissingObjectException : Exception
     {
+        /// <summary>
+        /// Initializes a new Simplic.Package.MissingObjectException insance.
+        /// </summary>
         public MissingObjectException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new Simplic.Package.MissingObjectException insance.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public MissingObjectException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new Simplic.Package.MissingObjectException insance.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="inner">The exception that caused this exception, or null.</param>
         public MissingObjectException(string message, Exception inner) : base(message, inner)
         {
         }
