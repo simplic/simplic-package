09312403d49be1112520ce43e1c910a80aa7a116
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-02 12:16:03 +0200

    Comment LogResult implementation

--- src/Simplic.Package/Model/InitializePackageSystemResult.cs
+++ src/Simplic.Package/Model/InitializePackageSystemResult.cs
@@ -1,5 +1,11 @@
 ﻿namespace Simplic.Package
 {
+    /// <summary>
+    /// Represents a result of a method which initializes the package system.
+    /// <para>
+    /// Here are no further information needed than in the base <see cref="LogResult"/>
+    /// </para>
+    /// </summary>
     public class InitializePackageSystemResult : LogResult
     {
     }
--- src/Simplic.Package/Model/UninstallObjectResult.cs
+++ src/Simplic.Package/Model/UninstallObjectResult.cs
@@ -1,7 +1,16 @@
 ﻿namespace Simplic.Package
 {
+    /// <summary>
+    /// Represents a result of a method which uninstalls a package.
+    /// <para>
+    /// Contains additional data rearding the success of the uninstalling process.
+    /// </para>
+    /// </summary>
     public class UninstallObjectResult : LogResult
     {
+        /// <summary>
+        /// Gets or sets wherther the uninstall was successful.
+        /// </summary>
         public bool Success { get; set; }
     }
 }
