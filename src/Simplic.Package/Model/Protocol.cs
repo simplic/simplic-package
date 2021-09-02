25fbc786ae4690bacd1f031d0dcdd717b5fcb2af
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-02 10:37:11 +0200

    Add Package comments

--- src/Simplic.Package/Model/Protocol.cs
+++ src/Simplic.Package/Model/Protocol.cs
@@ -2,8 +2,17 @@
 
 namespace Simplic.Package
 {
+    /// <summary>
+    /// Represents a protocol.
+    /// </summary>
     public class Protocol
     {
+        /// <summary>
+        /// Gets or sets the messages.
+        /// <para>
+        /// Will contain all logged messages of a executed package method.
+        /// </para>
+        /// </summary>
         public IList<ProtocolItem> Messages { get; set; } = new List<ProtocolItem>();
     }
 }
--- src/Simplic.Package/Model/ProtocolItem.cs
+++ src/Simplic.Package/Model/ProtocolItem.cs
@@ -8,22 +8,35 @@ namespace Simplic.Package
     public class ProtocolItem
     {
         /// <summary>
-        /// Gets or sets the log-datetime
+        /// Gets or sets the log-datetime.
+        /// <para>
+        /// The DateTime will indicate when the protocol item was created.
+        /// </para>
         /// </summary>
         public DateTime DateTime { get; set; } = DateTime.Now;
 
         /// <summary>
-        /// Gets or sets the message
+        /// Gets or sets the message.
+        /// <para>
+        /// Represents the message of the protocol item.
+        /// </para>
         /// </summary>
         public string Message { get; set; }
 
         /// <summary>
-        /// Gets or sets the log level
+        /// Gets or sets the log level.
+        /// <para>
+        /// The LogLevel of the message, indicating its severity.
+        /// </para>
         /// </summary>
         public LogLevel LogLevel { get; set; }
 
         /// <summary>
-        /// Gets or sets the associated exception
+        /// Gets or sets the associated exception.
+        /// <para>
+        /// Will contain the exception of the protocol item when an exception is given.
+        /// This might only happen in Error or debug cases.
+        /// </para>
         /// </summary>
         public Exception Exception { get; set; }
     }
