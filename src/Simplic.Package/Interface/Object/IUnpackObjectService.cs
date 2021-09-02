8b95ce7c70f745156ae624d0c7ac3d017c3cfda4
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-02 16:06:38 +0200

    Improve object interface comments

--- src/Simplic.Package/Interface/Object/IUnpackObjectService.cs
+++ src/Simplic.Package/Interface/Object/IUnpackObjectService.cs
@@ -3,15 +3,15 @@
 namespace Simplic.Package
 {
     /// <summary>
-    /// Service for unpacking (making them installable) objects
+    /// Service for unpacking (making them installable) objects,
     /// </summary>
     public interface IUnpackObjectService
     {
         /// <summary>
-        /// Unpacks an object and makes it installable
+        /// Unpacks an object and makes it installable,
         /// </summary>
-        /// <param name="extractArchiveEntryResult">The archive entry to unpack from</param>
-        /// <returns>A UnpackObjectResult object that is wrapping the InstallableObject</returns>
+        /// <param name="extractArchiveEntryResult">The archive entry to unpack from.</param>
+        /// <returns>A UnpackObjectResult object that is wrapping the InstallableObject.</returns>
         Task<UnpackObjectResult> UnpackObject(ExtractArchiveEntryResult extractArchiveEntryResult);
     }
 }
