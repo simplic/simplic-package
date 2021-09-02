cb20453ec734a12d7f519b0e317483b3736b66ff
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-02 14:21:15 +0200

    Enhance core interface comments

--- src/Simplic.Package/Interface/Core/ILogService.cs
+++ src/Simplic.Package/Interface/Core/ILogService.cs
@@ -9,16 +9,16 @@ namespace Simplic.Package
     public interface ILogService
     {
         /// <summary>
-        /// Subscribers to this event are invoked whenever a message is added to the log
+        /// Subscribers to this event are invoked whenever a message is added to the log.
         /// </summary>
         event EventHandler<LogMessageEventArgs> MessageAdded;
 
         /// <summary>
-        /// Adds a message to the log
+        /// Adds a message to the log.
         /// </summary>
-        /// <param name="message">The message to add</param>
-        /// <param name="logLevel">The LogLevel of the message, indicating its severity</param>
-        /// <param name="exception">The Exception that was caught</param>
+        /// <param name="message">The message to add.</param>
+        /// <param name="logLevel">The LogLevel of the message, indicating its severity.</param>
+        /// <param name="exception">The Exception that was caught.</param>
         Task WriteAsync(string message, LogLevel logLevel, Exception exception = null);
     }
 }
--- src/Simplic.Package/Interface/Core/IPackageTrackingRepository.cs
+++ src/Simplic.Package/Interface/Core/IPackageTrackingRepository.cs
@@ -4,38 +4,38 @@ using System.Threading.Tasks;
 namespace Simplic.Package
 {
     /// <summary>
-    /// Service to check and add package versions in the database
+    /// Service to check and add package versions in the database.
     /// </summary>
     public interface IPackageTrackingRepository
     {
         /// <summary>
-        /// Gets the installed version of a given package
+        /// Gets the installed version of a given package.
         /// </summary>
-        /// <param name="guid">The guid of the package</param>
-        /// <returns>The version or null if no version exists</returns>
+        /// <param name="guid">The guid of the package.</param>
+        /// <returns>The version or null if no version exists.</returns>
         Task<Version> GetPackageVersion(Guid guid);
 
         /// <summary>
-        /// Gets the installed version of a given package
+        /// Gets the installed version of a given package.
         /// </summary>
-        /// <param name="packageName">The name of the package</param>
-        /// <returns>The version or null if no version exists</returns>
+        /// <param name="packageName">The name of the package.</param>
+        /// <returns>The version or null if no version exists.</returns>
         Task<Version> GetPackageVersion(string packageName);
 
         /// <summary>
-        /// Adds the version of a given package to the database
+        /// Adds the version of a given package to the database.
         /// </summary>
-        /// <param name="package">The package whos version is to add</param>
-        /// <returns>Whether adding the version worked</returns>
+        /// <param name="package">The package whos version is to add.</param>
+        /// <returns>Whether adding the version worked.</returns>
         Task<bool> AddPackgageVersion(Package package);
 
         /// <summary>
-        /// Adds the version of a given package to the database
+        /// Adds the version of a given package to the database.
         /// </summary>
-        /// <param name="packageName">The name of the package</param>
-        /// <param name="guid">The guid of the package</param>
-        /// <param name="version">The version of the package</param>
-        /// <returns>Whether adding the version worked</returns>
+        /// <param name="packageName">The name of the package.</param>
+        /// <param name="guid">The guid of the package.</param>
+        /// <param name="version">The version of the package.</param>
+        /// <returns>Whether adding the version worked.</returns>
         Task<bool> AddPackgageVersion(string packageName, Guid guid, Version version);
     }
 }
