ba8ae08773fb6a237bacddcabf7ae8f725df22d9
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-06 16:08:27 +0200

    Add Combobox comments

--- src/Simplic.Package.ComboBox/Model/ComboBox.cs
+++ src/Simplic.Package.ComboBox/Model/ComboBox.cs
@@ -1,11 +1,33 @@
 ﻿namespace Simplic.Package.ComboBox
 {
+    /// <summary>
+    /// Represents the content of a combo box.
+    /// </summary>
     public class ComboBox : IContent
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sql statement.
+        /// </summary>
         public string SqlStatement { get; set; }
+
+        /// <summary>
+        /// Gets or sets the select column.
+        /// </summary>
         public string SelectColumn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the select column background.
+        /// </summary>
         public string SelectColumnBackground { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fs control name.
+        /// </summary>
         public string FsControlName { get; set; }
     }
 }
