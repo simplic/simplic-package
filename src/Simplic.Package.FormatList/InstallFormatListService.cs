3de37bde1705655047444305df435ff48a817fb0
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-07 16:50:51 +0200

    comment format lists services

--- src/Simplic.Package.FormatList/InstallFormatListService.cs
+++ src/Simplic.Package.FormatList/InstallFormatListService.cs
@@ -4,20 +4,29 @@ using Unity;
 
 namespace Simplic.Package.FormatList
 {
+    /// <summary>
+    /// Service to install format lists.
+    /// </summary>
     public class InstallFormatListService : IInstallObjectService
     {
         private readonly IObjectRepository repository;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="InstallFormatListService"/>.
+        /// </summary>
+        /// <param name="repository"></param>
         public InstallFormatListService([Dependency("formatList")] IObjectRepository repository)
         {
             this.repository = repository;
         }
 
+        /// <inheritdoc/>
         public async Task<InstallObjectResult> InstallObject(InstallableObject installableObject)
         {
             return await repository.InstallObject(installableObject);
         }
 
+        /// <inheritdoc/>
         public Task<UninstallObjectResult> UninstallObject(InstallableObject installableObject)
         {
             throw new NotImplementedException();
--- src/Simplic.Package.FormatList/PackFormatListService.cs
+++ src/Simplic.Package.FormatList/PackFormatListService.cs
@@ -1,7 +1,14 @@
 ﻿namespace Simplic.Package.FormatList
 {
+    /// <summary>
+    /// Service to pack format lists.
+    /// </summary>
     public class PackFormatListService : PackObjectServiceBase, IPackObjectService
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="PackFormatListService"/>.
+        /// </summary>
+        /// <param name="fileService"></param>
         public PackFormatListService(IFileService fileService) : base(fileService)
         {
         }
