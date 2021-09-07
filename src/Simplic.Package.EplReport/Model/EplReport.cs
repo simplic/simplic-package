2f18751c24ebe5c3c505edb7bd1b2b42a39e491b
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-07 08:22:02 +0200

    Comment epl report pocos

--- src/Simplic.Package.EplReport/Model/EplReport.cs
+++ src/Simplic.Package.EplReport/Model/EplReport.cs
@@ -3,15 +3,48 @@ using System;
 
 namespace Simplic.Package.EplReport
 {
+    /// <summary>
+    /// Represents the content of an epl report.
+    /// </summary>
     public class EplReport : IContent
     {
+        /// <summary>
+        /// Gets or sets the id.
+        /// <para>
+        /// Represents the unique identifier and primary key of an epl report.
+        /// </para>
+        /// </summary>
         [JsonProperty(Required = Required.Always)]
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the internal name.
+        /// </summary>
         public string InternalName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the report desing id.
+        /// </summary>
         public string ReportDesignId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the printer.
+        /// </summary>
         public string Printer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag whether the report is contextless printable.
+        /// </summary>
         public bool IsContextlessPrintable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the configuration.
+        /// </summary>
         public IEplReportConfiguration Configuration { get; set; }
     }
 }
--- src/Simplic.Package.EplReport/Model/SequenceConfiguration.cs
+++ src/Simplic.Package.EplReport/Model/SequenceConfiguration.cs
@@ -2,8 +2,14 @@
 
 namespace Simplic.Package.EplReport
 {
+    /// <summary>
+    /// Represents a sequece configuration of an epl report.
+    /// </summary>
     public class SequenceConfiguration : IEplReportConfiguration
     {
+        /// <summary>
+        /// Gets or sets the sequence id.
+        /// </summary>
         public Guid SequenceId { get; set; }
     }
 }
