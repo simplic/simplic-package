21f3280d506e2df8d354c055b54a4267a1e9a200
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-07 16:26:03 +0200

    comment format list pocos

--- src/Simplic.Package.FormatList/Model/FormatList.cs
+++ src/Simplic.Package.FormatList/Model/FormatList.cs
@@ -2,10 +2,24 @@
 
 namespace Simplic.Package.FormatList
 {
+    /// <summary>
+    /// Represents the package content of a format list.
+    /// </summary>
     public class FormatList : IContent
     {
+        /// <summary>
+        /// Gets or sets the internal name.
+        /// </summary>
         public string InternalName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the items.
+        /// </summary>
         public IList<FormatListItem> Items { get; set; } = new List<FormatListItem>();
     }
 }
