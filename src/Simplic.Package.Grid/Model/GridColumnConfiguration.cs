6e9a122202934245ab10cc1938a98017347b01c2
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-09 08:48:30 +0200

    comment grid configuration data classes

--- src/Simplic.Package.Grid/Model/GridColumnConfiguration.cs
+++ src/Simplic.Package.Grid/Model/GridColumnConfiguration.cs
@@ -2,10 +2,24 @@
 
 namespace Simplic.Package.Grid
 {
+    /// <summary>
+    /// Represents a grid column configuration.
+    /// </summary>
     public class GridColumnConfiguration : ColumnConfiguration
     {
+        /// <summary>
+        /// Gets or sets the grid cell sql highlights.
+        /// </summary>
         public IList<GridCellSqlHighlight> GridCellSqlHighlights { get; set; }
+
+        /// <summary>
+        /// Gets or sets the use for unique filtering flag.
+        /// </summary>
         public bool UseForUniqueFiltering { get; set; }
+
+        /// <summary>
+        /// Gets or sets hte split character.
+        /// </summary>
         public string SplitCharacter { get; set; }
     }
 }
--- src/Simplic.Package.Grid/Model/VirtualGroupDefinition.cs
+++ src/Simplic.Package.Grid/Model/VirtualGroupDefinition.cs
@@ -1,7 +1,13 @@
 ﻿namespace Simplic.Package.Grid
 {
+    /// <summary>
+    /// Represents a virtual group definition.
+    /// </summary>
     public class VirtualGroupDefinition
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
     }
 }
