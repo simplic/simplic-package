b6eb3682be081969c2967a11cd0e7ead62afdb70
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-13 16:56:46 +0200

    comment icon content

--- src/Simplic.Package.Icon/Model/IconContent.cs
+++ src/Simplic.Package.Icon/Model/IconContent.cs
@@ -3,9 +3,19 @@ using System;
 
 namespace Simplic.Package.Icon
 {
+    /// <summary>
+    /// Represnts the content of an icon.
+    /// </summary>
     public class IconContent : IContent
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the blob.
+        /// </summary>
         public byte[] Blob { get; set; }
     }
 }
