b21a636b026a5c649c331e0a21c01952582673eb
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-13 13:43:38 +0200

    comment grid services

--- src/Simplic.Package.Grid/PackGridService.cs
+++ src/Simplic.Package.Grid/PackGridService.cs
@@ -1,7 +1,14 @@
 ﻿namespace Simplic.Package.Grid
 {
+    /// <summary>
+    /// Service to pack a grid.
+    /// </summary>
     public class PackGridService : PackObjectServiceBase, IPackObjectService
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="PackGridService"/>.
+        /// </summary>
+        /// <param name="fileService"></param>
         public PackGridService(IFileService fileService) : base(fileService)
         {
         }
