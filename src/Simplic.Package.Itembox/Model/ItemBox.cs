55385129b816f2736be70dd5ad06ffe305eae34b
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-13 17:05:30 +0200

    comment itembox content

--- src/Simplic.Package.Itembox/Model/ItemBox.cs
+++ src/Simplic.Package.Itembox/Model/ItemBox.cs
@@ -1,13 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Simplic.Package.ItemBox
 {
+    /// <summary>
+    /// Represents the content of an itembox.
+    /// </summary>
     public class ItemBox : IContent
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the profiles.
+        /// </summary>
         public IList<ItemBoxProfile> Profiles { get; set; } = new List<ItemBoxProfile>();
     }
 }
