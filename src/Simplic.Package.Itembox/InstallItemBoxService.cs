1f91d36e0425c617c97148462c373fab59bbdbf5
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-14 09:00:18 +0200

    comment itembox services

--- src/Simplic.Package.Itembox/InstallItemBoxService.cs
+++ src/Simplic.Package.Itembox/InstallItemBoxService.cs
@@ -4,20 +4,29 @@ using Unity;
 
 namespace Simplic.Package.ItemBox
 {
+    /// <summary>
+    /// Service to install an itembox. 
+    /// </summary>
     public class InstallItemBoxService : IInstallObjectService
     {
         private readonly IObjectRepository repository;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="InstallItemBoxService"/>.
+        /// </summary>
+        /// <param name="repository"></param>
         public InstallItemBoxService([Dependency("itemBox")] IObjectRepository repository)
         {
             this.repository = repository;
         }
 
+        /// <inheritdoc/>
         public async Task<InstallObjectResult> InstallObject(InstallableObject installableObject)
         {
             return await repository.InstallObject(installableObject);
         }
 
+        /// <inheritdoc/>
         public Task<UninstallObjectResult> UninstallObject(InstallableObject installableObject)
         {
             throw new NotImplementedException();
