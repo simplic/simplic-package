6a8425b2fb68c42086d9af861a7092ca8002e3ee
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-14 10:29:03 +0200

    Comment repository content

--- src/Simplic.Package.Repository/Model/RepositoryContent.cs
+++ src/Simplic.Package.Repository/Model/RepositoryContent.cs
@@ -1,7 +1,13 @@
 ﻿namespace Simplic.Package.Repository
 {
+    /// <summary>
+    /// Represents the content of a repository.
+    /// </summary>
     internal class RepositoryContent : IContent
     {
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
         public byte[] Data { get; set; }
     }
 }
