7753e4a7236876b856f783f6f4a3c1f4c7e49a51
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-16 16:23:24 +0200

    comment stack register content classes

--- src/Simplic.Package.StackRegister/Model/SqlConfiguration.cs
+++ src/Simplic.Package.StackRegister/Model/SqlConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿namespace Simplic.Package.StackRegister
 {
+    /// <summary>
+    /// Represents the content of a sql stack register configuration.
+    /// </summary>
     public class SqlConfiguration : IStackRegisterConfiguration
     {
         public string Statement { get; set; }
