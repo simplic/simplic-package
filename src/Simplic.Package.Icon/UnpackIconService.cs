eec1d106806765127e15ce382c0cb5dbf6917b68
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-24 13:33:21 +0200

    change unpack icon  service to extract the guid form the filename

--- src/Simplic.Package.Icon/UnpackIconService.cs
+++ src/Simplic.Package.Icon/UnpackIconService.cs
@@ -1,9 +1,38 @@
-﻿namespace Simplic.Package.Icon
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Simplic.Package.Icon
 {
     /// <summary>
     /// Service to unpack an icon.
     /// </summary>
-    public class UnpackIconService : UnpackObjectServiceBase<IconContent>
+    public class UnpackIconService : IUnpackObjectService
     {
+        /// <inheritdoc/>
+        public async Task<UnpackObjectResult> UnpackObject(ExtractArchiveEntryResult extractArchiveEntryResult)
+        {
+            var filename = Path.GetFileName(extractArchiveEntryResult.Location);
+
+            var installableObject = new InstallableObject
+            {
+                Content = new IconContent
+                {
+                    Guid = Guid.Parse(filename.Split('#').First()),
+                    Name = filename.Split('#').Last(),
+                    Blob = extractArchiveEntryResult.Data
+                },
+                Target = extractArchiveEntryResult.Location,
+                Mode = extractArchiveEntryResult.Mode
+            };
+
+            return new UnpackObjectResult
+            {
+                InstallableObject = installableObject,
+                Message = $"Unpacked icon at {extractArchiveEntryResult.Location}.",
+                LogLevel = LogLevel.Info
+            };
+        }
     }
 }
