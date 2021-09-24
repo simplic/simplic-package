11169d1630e1d412ff9dd8d55f9a3283eefaa3a7
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-09-24 11:35:15 +0200

    update unpack icon service to use abstract class

--- src/Simplic.Package.Icon/UnpackIconService.cs
+++ src/Simplic.Package.Icon/UnpackIconService.cs
@@ -1,33 +1,9 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-
-namespace Simplic.Package.Icon
+﻿namespace Simplic.Package.Icon
 {
     /// <summary>
     /// Service to unpack an icon.
     /// </summary>
-    public class UnpackIconService : IUnpackObjectService
+    public class UnpackIconService : UnpackObjectServiceBase<IconContent>
     {
-        /// <inheritdoc/>
-        public async Task<UnpackObjectResult> UnpackObject(ExtractArchiveEntryResult extractArchiveEntryResult)
-        {
-            var installableObject = new InstallableObject
-            {
-                Content = new IconContent
-                {
-                    Name = extractArchiveEntryResult.Location.Split('/').Last(),
-                    Blob = extractArchiveEntryResult.Data
-                },
-                Target = extractArchiveEntryResult.Location,
-                Mode = extractArchiveEntryResult.Mode
-            };
-
-            return new UnpackObjectResult
-            {
-                InstallableObject = installableObject,
-                Message = $"Unpacked icon at {extractArchiveEntryResult.Location}.",
-                LogLevel = LogLevel.Info
-            };
-        }
     }
 }
