7b7cdf4ccf5755ae72e77d9aea4783d7d465a6b7
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-11-19 18:41:45 +0100

    restrict attribute usage

--- src/Simplic.Package/Attributes/PackageExtensionAttribute.cs
+++ src/Simplic.Package/Attributes/PackageExtensionAttribute.cs
@@ -5,6 +5,7 @@ namespace Simplic.Package.Attributes
     /// <summary>
     /// Attribute to define the entry point of a package system extension.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
     public class PackageExtensionAttribute : Attribute
     {
     }
