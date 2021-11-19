2bf3153afda0cb5a003b1b0a9db6d65748300626
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-11-19 18:36:54 +0100

    Add package extension service

---
+++ src/Simplic.Package/Attributes/PackageExtensionAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Simplic.Package.Attributes
+{
+    /// <summary>
+    /// Attribute to define the entry point of a package system extension.
+    /// </summary>
+    public class PackageExtensionAttribute : Attribute
+    {
+    }
+}
---
+++ src/Simplic.Package/Interface/Core/IExtensionService.cs
@@ -0,0 +1,14 @@
+﻿namespace Simplic.Package
+{
+    /// <summary>
+    /// Service to load package extensions.
+    /// </summary>
+    public interface IExtensionService
+    {
+        /// <summary>
+        /// Loads a package extension.
+        /// </summary>
+        /// <param name="package"></param>
+        void LoadExtensions(Package package);
+    }
+}
