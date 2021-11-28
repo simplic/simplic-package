bc789c0562282815bb4ee3793c0ddb0c8853a897
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-11-28 10:31:38 +0100

    Add new project and models

---
+++ src/Simplic.Package.Configuration/Model/ConfigurationValueSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Package.Configuration.Model
+{
+    /// <summary>
+    /// Defines the source of the value.
+    /// </summary>
+    public enum ConfigurationValueSource
+    {
+        /// <summary>
+        /// A package value is the value in the package.json.
+        /// </summary>
+        PackageValue = 0,
+
+        /// <summary>
+        /// A request value is requested in every installation.
+        /// </summary>
+        RequestValue = 1
+    }
+}
