a645b425a5edbe95b2296c600e15e7c4ac456964
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-01-02 18:03:39 +0100

    add unpack service

---
+++ src/Simplic.Package.IronPythonScript/UnpackScriptService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Simplic.Package.IronPythonScript
+{
+    /// <summary>
+    /// Service to unpack script services
+    /// </summary>
+    public class UnpackScriptService : IUnpackObjectService
+    {
+        /// <inheritdoc/>
+        public Task<UnpackObjectResult> UnpackObject(ExtractArchiveEntryResult extractArchiveEntryResult)
+        {
+            var installableObject = new InstallableObject
+            {
+                Content = new IronPythonScript
+                {
+                    Script = extractArchiveEntryResult.Data.ToString()
+                },
+                Target = extractArchiveEntryResult.Location,
+                Mode = extractArchiveEntryResult.Mode
+            };
+
+            return Task.FromResult(new UnpackObjectResult
+            {
+                InstallableObject = installableObject,
+                Message = $"Unpacked repository at {extractArchiveEntryResult.Location}",
+                LogLevel = LogLevel.Info
+            });
+        }
+    }
+}
