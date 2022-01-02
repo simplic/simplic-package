9d47bb238ff3612723b72f57b5a74b0973d583a2
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-01-02 17:34:23 +0100

    add python script execution base

---
+++ src/Simplic.Package.IronPythonScript/InstallScriptService.cs
@@ -0,0 +1,40 @@
+﻿using Simplic.Framework.Core;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simplic.Package.IronPythonScript
+{
+    /// <summary>
+    /// Service to install an iron python script.
+    /// </summary>
+    public class InstallScriptService : IInstallObjectService
+    {
+        /// <inheritdoc/>
+        public Task<InstallObjectResult> InstallObject(InstallableObject installableObject)
+        {
+            try
+            {
+                if (installableObject.Content is IronPythonScript script)
+                {
+                    GlobalDlrHost.Host.DefaultScope.Execute(script.Script);
+                    var classname = Path.GetFileName(installableObject.Target);
+                    var classInstance = GlobalDlrHost.Host.DefaultScope.CreateClassInstance(classname);
+
+                    classInstance.Instance.Execute();
+                }
+                return Task.FromResult(new InstallObjectResult { Success = true });
+            }
+            catch
+            {
+                return Task.FromResult(new InstallObjectResult { Success = false });
+            }
+        }
+
+        /// <inheritdoc/>
+        public Task<UninstallObjectResult> UninstallObject(InstallableObject installableObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
